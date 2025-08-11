ecb7897f1730af659fdc025cc4f0797a339de111
Nondescript_Potato <ethan.mcconnell20@gmail.com>
2025-08-10 22:12:58 -0500

    Event Loop Working

--- src/main.rs
+++ src/main.rs
@@ -4,37 +4,55 @@ mod errors;
 mod playback;
 mod keyboard;
 mod actions;
+
 // -- Imports -- //
 
+use rodio::Source;
 use playback::Player;
+use actions::Action;
 use keyboard::{KeyboardWatcher, KeyDirInput};
 
 // -- Main -- //
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-	let mut kw: KeyboardWatcher<()> = KeyboardWatcher::new();
-	kw.insert((), T_COMBO);
+	let player = Player::new("BlackHole 16ch")?;
+	let src = rodio::Decoder::new_mp3(
+		std::fs::File::open("media/audio/samples/fireball.mp3")?
+	)?.buffered();
+	
+	
+	let mut kw: KeyboardWatcher<Action<()>> = KeyboardWatcher::new();
+	kw.insert(Action::Quit, QUIT_COMBO);
+	kw.insert(Action::Mute, MUTE_COMBO)	;
+	kw.insert(Action::Unmute, UNMUTE_COMBO);
+	kw.insert(Action::Play(()), PLAY_COMBO);
 	
 	loop {
 		let sequence = kw.listen_for_combo();
-		if let Some(_) = kw.get(sequence) { break }
+		let Some(act) = kw.get(sequence) else { continue };
+		
+		match act {
+			Action::Quit => break,
+			Action::Mute => player.get_sink().set_volume(0.0),
+			Action::Unmute => player.get_sink().set_volume(1.0),
+			Action::Play(()) => player.play_audio(src.clone()),
+		}
 	}
-	
-	println!("Correct Combo!");
-	
-	let player = Player::new("BlackHole 16ch")?;
-	
-	let src = rodio::Decoder::new_mp3(
-		std::fs::File::open("media/audio/samples/fireball.mp3")?
-	)?;
-	
-	player.play_audio(src);
-	
-	player.get_sink().sleep_until_end();
 	
 	Ok(())
 }
 
 // -- Testing -- //
 
-const T_COMBO: [KeyDirInput; 4] = [KeyDirInput::Up, KeyDirInput::Down, KeyDirInput::Left, KeyDirInput::Right];
+const QUIT_COMBO: [KeyDirInput; 5] = [
+	KeyDirInput::Up,
+	KeyDirInput::Right,
+	KeyDirInput::Down,
+	KeyDirInput::Down,
+	KeyDirInput::Down,
+];
+
+const MUTE_COMBO: [KeyDirInput; 2] = [KeyDirInput::Up, KeyDirInput::Up];
+const UNMUTE_COMBO: [KeyDirInput; 2] = [KeyDirInput::Down, KeyDirInput::Down];
+
+const PLAY_COMBO: [KeyDirInput; 2] = [KeyDirInput::Left, KeyDirInput::Right];
